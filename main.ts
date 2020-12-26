6a05393f0c53ed03ea475a4a832b022ff42289ff
Anicai Ovidiu <ovi@0v1.ro>
2020-12-26 11:58:23 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,19 +2,24 @@ radio.onReceivedNumber(function (receivedNumber) {
     straight(receivedNumber)
     basic.pause(Pause)
 })
+function stop () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P8, 0)
+    pins.analogWritePin(AnalogPin.P16, 0)
+    pins.analogWritePin(AnalogPin.P12, 0)
+}
+radio.onReceivedString(function (receivedString) {
+    if ("" == "") {
+        stop()
+    }
+})
 function straight (num: number) {
     if (num > 0) {
         pins.analogWritePin(AnalogPin.P0, num)
         pins.analogWritePin(AnalogPin.P8, num)
-        basic.pause(Pause)
-        pins.analogWritePin(AnalogPin.P0, 0)
-        pins.analogWritePin(AnalogPin.P8, 0)
     } else {
         pins.analogWritePin(AnalogPin.P16, num * -1)
         pins.analogWritePin(AnalogPin.P12, num * -1)
-        basic.pause(Pause)
-        pins.analogWritePin(AnalogPin.P16, 0)
-        pins.analogWritePin(AnalogPin.P12, 0)
     }
 }
 let Pause = 0

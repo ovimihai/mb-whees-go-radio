e1f08639da6574471d8a9956aa522b1f71e35556
Anicai Ovidiu <ovi@0v1.ro>
2020-12-26 10:50:23 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,23 @@
 radio.onReceivedNumber(function (receivedNumber) {
     straight(receivedNumber)
+    basic.pause(Pause)
 })
 function straight (num: number) {
     if (num > 0) {
         pins.analogWritePin(AnalogPin.P0, num)
         pins.analogWritePin(AnalogPin.P8, num)
-        basic.pause(1000)
+        basic.pause(Pause)
         pins.analogWritePin(AnalogPin.P0, 0)
         pins.analogWritePin(AnalogPin.P8, 0)
     } else {
         pins.analogWritePin(AnalogPin.P16, num * -1)
         pins.analogWritePin(AnalogPin.P12, num * -1)
-        basic.pause(1000)
+        basic.pause(Pause)
         pins.analogWritePin(AnalogPin.P16, 0)
         pins.analogWritePin(AnalogPin.P12, 0)
     }
 }
+let Pause = 0
 radio.setGroup(1)
 basic.showIcon(IconNames.Diamond)
+Pause = 100
